d8757da09eadc84f2309843cd5db0869abe87e3b
Johannes Sjoberg <johannes@proxy.com>
2021-04-28 12:10:04 +0200

    Fix: Allow for object with buffer property

--- test/buffer_source_converter.ts
+++ test/buffer_source_converter.ts
@@ -26,6 +26,11 @@ context("BufferSourceConverter", () => {
     assert.strictEqual(Convert.ToHex(data), vectorHex);
   });
 
+  it("convert from object with ArrayBuffer buffer property", () => {
+    const data = BufferSourceConverter.toUint8Array({ buffer: vector } as ArrayBufferView);
+    assert.strictEqual(Convert.ToHex(data), vectorHex);
+  });
+
   it("convert from offset to Uint8Array", () => {
     const source = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
     const offset = source.subarray(5, 10);
